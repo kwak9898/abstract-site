3d87319895b88668ed9bc498c2c1e559dcfa2f2f
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2021-12-03 22:49:55 +0900

    항해99 주특기 심화 개인 프로젝트
    항해99 주특기 심화 개인 프로젝트

--- src/index.js
+++ src/index.js
@@ -3,15 +3,75 @@ class Site {
         this.boards = [];
     }
     addBoard(board) {
+        if (this.boards.find((boardInfo) => boardInfo.name === board.name)) {
+            throw Error()
+        }
+        board.myboard = true
         this.boards.push(board);
+    }
+    findBoardByName(name) {
+        return this.boards.find((board) => board.name === name)
     }
 }
 
-class Board { }
+class Board {
+    constructor(name) {
+        if (name == null || name == "") {
+            throw Error()
+        }
+        this.name = name
+        this.myboard = false
+        this.articles = []
+    }
+    publish(article) {
+        if (this.myboard == false) {
+            throw Error()
+        }
+        article.id = `${this.name}-${Math.random()}`
+        article.createdDate = new Date().toISOString()
 
-class Article { }
+        this.articles.push(article)
+    }
+    getAllArticles() {
+        return this.articles
+    }
+}
 
-class Comment { }
+class Article {
+    constructor(article) {
+        let { subject, content, author } = article
+
+        if (subject == null || subject == "" || content == null || content == "" || author == null || author == "") {
+            throw Error();
+        }
+
+        this.subject = subject
+        this.content = content
+        this.author = author
+        this.comments = []
+    }
+    reply(comment) {
+        if (!this.id) {
+            throw Error()
+        }
+        comment.createdDate = new Date().toISOString()
+        this.comments.push(comment)
+    }
+    getAllComments() {
+        return this.comments
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        let { content, author } = comment
+        if (content == null || content == "" || author == null || author == "") {
+            throw Error()
+        }
+        this.content
+        this.author
+    }
+}
 
 module.exports = {
     Site,
